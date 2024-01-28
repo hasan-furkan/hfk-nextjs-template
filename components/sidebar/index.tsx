c9fe0bc987f91c34dc7da39063a536c00d3b96fd
hasan-furkan <hsnfrkn32@gmail.com>
2024-01-28 03:42:31 +0100

    add sidebar down svg and sidebar menu svg

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -10,15 +10,16 @@ const SideBarComponent = () => {
                         <Image src="/images/logo.jpeg" alt="logo" width={64} height={64} className="ml-2 mt-2 rounded-3xl" />
                         <h1 className="text-5xl text-gray-50 font-bold mr-2 mt-2">HFK</h1>
                         <div className="absolute right-[-18px] top-7 cursor-pointer bg-purple-300 rounded-3xl p-1">
-                            <Image src="/svgs/sidebarRight.svg" alt="sidebarRight" width={24} height={24} className="text-l" />
+                            <Image src="/svgs/sidebarMenu.svg" alt="sidebarRight" width={24} height={24} className="text-l" />
                         </div>
                     </div>
                 </div>
                 <div className="flex flex-col p-1 overflow-y-auto scroll-smooth">
-                   <div className="text-gray-50 mt-2 rounded-3xl gap-2">
-                       <div className="flex w-full ml-1 p-1 my-2 bg-purple-300 rounded-3xl">
+                   <div className="text-gray-50 mt-2 rounded-3xl gap-2 relative">
+                       <div className="flex w-full ml-1 p-1 my-2 bg-purple-300 rounded-3xl items-center">
                            <Image src="/svgs/home.svg" alt="home" width={28} height={28} className="ml-2" />
                            <h1 className="text-2xl font-bold ml-2">Home</h1>
+                           <Image src="/svgs/sidebarRight.svg" alt="sidebarRight" width={24} height={24} className="absolute right-1" />
                        </div>
                        <div className="flex w-full ml-1 p-1 my-2 bg-purple-300 rounded-3xl">
                            <Image src="/svgs/home.svg" alt="home" width={28} height={28} className="ml-2" />
